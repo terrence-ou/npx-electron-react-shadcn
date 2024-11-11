4f3d2aaad78667d262b2d954bdb713e2b66a86a0
terrence-ou <outerrencedl@gmail.com>
2024-11-11 12:23:25 -0500

    project init refactored

--- index.js
+++ index.js
@@ -1,50 +1,67 @@
 #! /usr/bin/env node
 
-const simpleGit = require("simple-git");
 const fs = require("fs");
 const path = require("path");
-const readline = require("readline");
+const simpleGit = require("simple-git");
+const prompts = require("prompts");
+const { validateProjectName, writePackage } = require("./utils");
 
 const repoUrl = "https://github.com/terrence-ou/electron-react-shadcn.git";
-const targetFolderName = process.argv[2] || "electron-react-shadcn";
-const targetDir = path.resolve(process.cwd(), targetFolderName);
 
-async function prompt(question) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  return new Promise((resolve) =>
-    rl.question(question, (answer) => {
-      rl.close();
-      resolve(answer.toLocaleLowerCase() || "y");
-    })
-  );
-}
+// Prompt questions
+const initConfirmQuestion = {
+  type: "text",
+  name: "confirm",
+  message:
+    "This will create a project in the current directory. Continue? (Y/n)",
+  initial: "Y",
+  validate: (value) =>
+    value.toLowerCase() === "y" || value.toLowerCase() === "n"
+      ? true
+      : "The input should be either y or n",
+};
 
+const questions = [
+  {
+    type: "text",
+    name: "projectName",
+    message: "Please enter your project name.",
+    initial: "electron-react-shadcn",
+    validate: (value) =>
+      validateProjectName(value) ? true : "Please enter a valid project namel",
+  },
+];
+
+// Setting up project and clone the repo
 async function cloneRepo() {
   try {
     // ask user to confirm creating the project in the current directory
-    const confirm = await prompt(
-      "This will create a project in the current directory. Continue? (Y/n)"
-    );
-    if (confirm !== "y") {
-      console.log("Process aborted");
+    const { confirm } = await prompts(initConfirmQuestion);
+    if (confirm.toLowerCase() != "y") {
+      console.log("Initialization aborted.");
       return;
     }
-    console.log("Creating electron app in: ", targetDir);
+
+    // Prompts for setting up the project
+    const { projectName } = await prompts(questions);
 
     // get project template
     console.log("Getting project template...");
+    const targetDir = path.resolve(process.cwd(), projectName);
     await simpleGit().clone(repoUrl, targetDir);
+
+    // Update package.json
+    writePackage(targetDir, projectName);
+
     // remove .git folder
     fs.rm(`${targetDir}/.git`, { recursive: true }, (err) => {
       if (err) console.error("failed to remove .git folder. Error: ", err);
     });
+
     console.log(
       "Project setup complete, please use the following commands to continue:"
     );
-    console.log(`\ncd ${targetFolderName}`);
+    console.log(`\ncd ${[projectName]}`);
     console.log("npm install && npm run dev\n");
   } catch (error) {
     console.error("Failed to get the project template. Error: ", error);
